544633ec1874a652181ac3706c4856a69db9e1ea
Frank <frank051284@gmail.com>
2019-02-12 15:24:38 +0800

    Startup a Double arms Programming in c#

---
+++ DoubleArms/Arm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleArms
+{
+    class Arm
+    {
+        X_axis xaxis;
+        Z_axis zaxis;
+
+        Arm() {
+            xaxis = X_axis.getXaxis();
+            zaxis = Z_axis.getZaxis();
+        }
+
+        public static Arm CreateArm() {
+            return new Arm();
+        }
+        //TODO: 手臂會有的功能 UP Down Left Right Seize Release Create Remove Show Hide 
+        void UP() {
+            zaxis.z--;
+        
+        }
+        void Down() {
+            zaxis.z++;
+        }
+        void Left() {
+            xaxis.x--;
+            zaxis.x = xaxis.x;
+        
+        }
+        void Right() {
+            xaxis.x++;
+            zaxis.x = xaxis.x;        
+        }
+        void Seize() { }
+        void Release() { }
+
+        void Create() { }
+        void Remove() { }
+        
+        void Show() { }
+        void Hide() { }
+    }
+
+    class X_axis{
+        //x軸的控制點為和z軸交接的地方
+        public int x;
+        public const int z = 100;
+        X_axis(){
+        
+        }
+
+        public static X_axis getXaxis()
+        {
+            return new X_axis();
+        }
+
+
+    
+    }
+    class Z_axis{
+        public int x, z;
+        Z_axis() { }
+        public static Z_axis getZaxis()
+        {
+            return new Z_axis();
+        }
+        
+    }
+}
+        
---
+++ DoubleArms/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoubleArms
+{
+    public partial class Form1 : Form
+    {
+        private int arm1_x, arm1_y;
+        private int arm1_width, arm1_height;
+
+        Arm arm;
+
+
+        //enum Position
+        //{
+        //    Left, Right, Up, Down
+        //}
+        //private Position _objPosition;
+
+        public Form1()
+        {
+            InitializeComponent();
+            arm1_x = 0;
+            arm1_y = 100;
+            arm1_width = 100;
+            arm1_height = 30;
+        }
+
+        private void ForArm1_Tick(object sender, EventArgs e)
+        {
+            arm1_width += 1;
+            Console.WriteLine("000000000000000000");
+            Invalidate();
+        }
+
+        private void FormView_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.BlueViolet, arm1_x, arm1_y, arm1_width, arm1_height);
+        }
+    }
+}
